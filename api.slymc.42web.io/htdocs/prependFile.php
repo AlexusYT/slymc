48857fba09365703be9ecccb39ac0d24140f327b
AlexusYT <alexus_xx@mail.ru>
2022-02-04 11:53:09 +0500

    Оптимизация кода получения списка серверов Рефакторинг

--- api.slymc.42web.io/htdocs/prependFile.php
+++ api.slymc.42web.io/htdocs/prependFile.php
@@ -1,8 +1,9 @@
 <?php
 
-if (!defined('MAIN_PATH'))  define('MAIN_PATH', substr(__DIR__, 0, strpos(__DIR__, "epiz_30774026")+13) ."/htdocs/");
+if (!defined('ROOT_PATH'))  define('ROOT_PATH', substr(__DIR__, 0, strpos(__DIR__, "epiz_30774026")+13) ."/htdocs/");
 if (!defined('API_PATH'))  define('API_PATH', substr(__DIR__, 0, strpos(__DIR__, "htdocs")+6) . DIRECTORY_SEPARATOR);
 
-require_once MAIN_PATH."utils/Utils.php";
-require_once MAIN_PATH."data/Token.php";
+require_once ROOT_PATH."utils/Utils.php";
+require_once ROOT_PATH."data/Token.php";
+require_once ROOT_PATH."data/User.php";
 require_once API_PATH."APIUtils.php";
--- api.slymc.42web.io/htdocs/servers/getServersInfo.php
+++ api.slymc.42web.io/htdocs/servers/getServersInfo.php
@@ -1,52 +1,42 @@
 <?php
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$conn = new mysqli( 'sql209.epizy.com', 'epiz_30774026', 'x6hyKkXWBV', 'epiz_30774026_slymc');
 
-$conn->set_charset("utf8mb4");
+include_once "../prependFile.php";
 
 //получение инфы о серверах
-$serversInformation = array();
-foreach ($conn->query("SELECT * FROM `servers` INNER JOIN `modpacks` ON servers.modPackID = modpacks.modPackID INNER JOIN `serverStats` ON servers.statsID = serverStats.statID")->fetch_all(MYSQLI_ASSOC) as $server) {
-    $serverInfo = ['serverName' => $server['serverName']];
-    $serverInfo += ['serverIP' => $server['serverIP']];
-    $serverInfo += ['serverPort' => intval($server['serverPort'])];
-    $serverInfo += ['modPackName' => $server['modPackName']];
-    $serverInfo += ['serverDescription' => $server['serverDescription']];
-    $serverInfo += ['serverThumbnail' => $server['serverThumbnail']];
-    //Моды:
-    $serverInfo += ['modList' => $modsInfo = getMods($conn, $server['modPackID'])];
-    //Игроки:
-    $serverInfo += ['playersOnline' => getPlayers($conn, $server['serverID'])];
-    //Статистика:
-    $stats = array();
-    $stats[] = ['statName' => 'status', 'statValue' => $server['status']];
-    $stats[] = ['statName' => 'tps', 'statValue' => intval($server['tps'])];
-    $stats[] = ['statName' => 'uptime', 'statValue' => intval($server['uptime'])];
-    $stats[] = ['statName' => 'reboot', 'statValue' => intval($server['reboot'])];
-    $stats[] = ['statName' => 'lastWipe', 'statValue' => intval($server['lastWipe'])];
-    $stats[] = ['statName' => 'nextWipe', 'statValue' => intval($server['nextWipe'])];
-    $stats[] = ['statName' => 'maxOnline', 'statValue' => intval($server['maxOnline'])];
-    $serverInfo += ['stats' => $stats];
-    $serversInformation[] = $serverInfo;
+$serversInformation = [];
+foreach (Utils::getDb()->getAll("SELECT * FROM `servers` INNER JOIN `modpacks` ON servers.modPackID = modpacks.modPackID") as $server) {
+	$serverInfo = [];
+	foreach ($server as $item => $value){
+		if($item==="modPackID")//моды
+			$serverInfo += ['modList' => Utils::getDb()->getAll("SELECT mods.name, mods.version, mods.description FROM mods INNER JOIN modLists ON modLists.modID = mods.modID WHERE modPackID = '?s'", $server['modPackID'])];
+		else if($item==="serverID")//игроки
+			$serverInfo += ['playersOnline' => getPlayers($value)];
+		else if($item==="statsID") $serverInfo += ['stats' => getStats($value)];//статы
+		else if($item==="serverPort") $serverInfo += ['serverPort' => intval($value)];
+		else $serverInfo += [$item => $value];
+	}
+	$serversInformation[] = $serverInfo;
+}
+header('Content-Type: application/json');
+echo json_encode(['servers' => $serversInformation]); //парсинг в json.
+
+function getStats(int $statsId): array
+{
+	$stats = [];
+	foreach (Utils::getDb()->getRow("SELECT * FROM serverStats WHERE serverStats.statsID = '?s'", $statsId) as $item => $value){
+		if($item === "statsID") continue;
+		$newValue = intval($value);
+		if(strval($newValue)!==$value) $newValue = $value;
+		$stats[] = ["statName" => $item, "statValue" => $newValue];
+	}
+	return $stats;
 }
 
-echo json_encode(array('servers' => $serversInformation)); //парсинг в json.
-$conn->close();
-
-
-function getPlayers(mysqli $conn, int $serverId): array
+function getPlayers(int $serverId): array
 {
-	$defaultHead = base64_encode(file_get_contents("../assets/steveHead.png"));
-
-    $playersInfo = array();
-    foreach ($conn->query("SELECT users.roleID, users.displayUsername, users.head FROM playersOnServers INNER JOIN users ON playersOnServers.userID = users.userID WHERE serverID = $serverId AND onServer = 1 ORDER BY users.roleID ASC")->fetch_all(MYSQLI_ASSOC) as $players){
-		if($players['head'] === "-") $players['head'] = $defaultHead;
-        $playersInfo[] = $players;
-    }
-    return $playersInfo;
-}
-
-function getMods(mysqli $conn, int $modpackId): array
-{
-	return $conn->query("SELECT mods.name, mods.version, mods.description FROM mods INNER JOIN modLists ON modLists.modID = mods.modID WHERE modPackID = $modpackId")->fetch_all(MYSQLI_ASSOC);
+	$playersInfo = [];
+	foreach (User::getOnlineUsers($serverId) as $player){
+		$playersInfo[] = ["roleID" => $player->getRole(), "displayUsername" => $player->getDisplayUsername(), "head" => $player->getHead()];
+	}
+	return $playersInfo;
 }
